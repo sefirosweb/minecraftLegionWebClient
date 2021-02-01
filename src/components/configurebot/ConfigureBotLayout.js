9218c3c6b0a3e8def9064e26863448b186c576fa
sefirosweb <sefirosweb@gmail.com>
2021-02-01 22:50:29 +0100

    Fixing bugs with path

--- src/components/configurebot/ConfigureBotLayout.js
+++ src/components/configurebot/ConfigureBotLayout.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react'
 import { NavLink } from 'react-router-dom'
 
-
 class ConfigureBotLayout extends React.Component {
     render() {
         return (
@@ -12,12 +11,12 @@ class ConfigureBotLayout extends React.Component {
                 <div className='row'>
                     <div className='col-12'>
                         <ul className="nav nav-tabs bg-dark">
-                            <li className="nav-item"><NavLink className="nav-link" activeClassName='active' to={`/configurebot/${this.props.socketId}/generalconfig`}>General Configuration</NavLink></li>
-                            <li className="nav-item"><NavLink className="nav-link" activeClassName='active' to={`/configurebot/${this.props.socketId}/itemstobeready`}>Items To Be Ready</NavLink></li>
-                            <li className="nav-item"><NavLink className="nav-link" activeClassName='active' to={`/configurebot/${this.props.socketId}/chests`}>Chests</NavLink></li>
-                            <li className="nav-item"><NavLink className="nav-link" activeClassName='active' to={`/configurebot/${this.props.socketId}/combat`}>Combat</NavLink></li>
-                            <li className="nav-item"><NavLink className="nav-link" activeClassName='active' to={`/configurebot/${this.props.socketId}/guardjob`}>Guard Job</NavLink></li>
-                            <li className="nav-item"><NavLink className="nav-link" activeClassName='active' to={`/configurebot/${this.props.socketId}/minerjob`}>Miner Job</NavLink></li>
+                            <li className="nav-item"><NavLink className="nav-link" activeClassName='active' to={`/${this.props.socketId}/generalconfig`}>General Configuration</NavLink></li>
+                            <li className="nav-item"><NavLink className="nav-link" activeClassName='active' to={`/${this.props.socketId}/itemstobeready`}>Items To Be Ready</NavLink></li>
+                            <li className="nav-item"><NavLink className="nav-link" activeClassName='active' to={`/${this.props.socketId}/chests`}>Chests</NavLink></li>
+                            <li className="nav-item"><NavLink className="nav-link" activeClassName='active' to={`/${this.props.socketId}/combat`}>Combat</NavLink></li>
+                            <li className="nav-item"><NavLink className="nav-link" activeClassName='active' to={`/${this.props.socketId}/guardjob`}>Guard Job</NavLink></li>
+                            <li className="nav-item"><NavLink className="nav-link" activeClassName='active' to={`/${this.props.socketId}/minerjob`}>Miner Job</NavLink></li>
                         </ul>
                     </div>
                 </div>
--- src/components/configurebot/ConfigureBotRoute.js
+++ src/components/configurebot/ConfigureBotRoute.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import { getBotIndexBySocketId } from '../../actions/botsAction'
-
+import NotFound from '../../pages/NotFound'
 
 import GeneralConfig from './GeneralConfig'
 import ItemsToBeReady from './ItemsToBeReady'
@@ -13,48 +13,49 @@ import MinerJob from './MinerJob'
 import ConfigureBotLayout from './ConfigureBotLayout'
 
 class ConfigureBotRoute extends React.Component {
-    // componentDidUpdate(prevProps) {
-    //     if (
-    //         this.props.botsOnline !== prevProps.botsOnline ||
-    //         this.props.loged !== prevProps.loged
-    //     ) {
-    //         this.checkCurrentBotIsConnected()
-    //     }
-    // }
+    componentDidUpdate(prevProps) {
+        if (
+            this.props.botsOnline !== prevProps.botsOnline ||
+            this.props.loged !== prevProps.loged
+        ) {
+            this.checkCurrentBotIsConnected()
+        }
+    }
 
 
-    // checkCurrentBotIsConnected = () => {
-    //     if (!this.props.loged) {
-    //         this.props.history.push('/configuration')
-    //         return
-    //     }
+    checkCurrentBotIsConnected = () => {
+        if (!this.props.loged) {
+            this.props.history.push('/configuration')
+            return
+        }
 
-    //     const { socketId } = this.props.match.params
-    //     if (this.props.getBotIndexBySocketId(socketId) < 0 && socketId !== undefined) {
-    //         console.log('Bot not found')
-    //         this.props.history.push('/dashboard')
-    //         return
-    //     }
-    // }
+        const { socketId } = this.props.match.params
+        if (this.props.getBotIndexBySocketId(socketId) < 0 && socketId !== undefined) {
+            console.log('Bot not found')
+            this.props.history.push('/dashboard')
+            return
+        }
+    }
 
-    // componentDidMount() {
-    //     this.checkCurrentBotIsConnected()
-    // }
+    componentDidMount() {
+        this.checkCurrentBotIsConnected()
+    }
 
     render() {
         return (
-
-            <ConfigureBotLayout socketId={this.props.match.params.socketId}>
-                <Switch>
-                    <Route exact path='/configurebot/:socketId/generalconfig' component={GeneralConfig} />
-                    <Route exact path='/configurebot/:socketId/itemstobeready' component={ItemsToBeReady} />
-                    <Route exact path='/configurebot/:socketId/chests' component={Chests} />
-                    <Route exact path='/configurebot/:socketId/combat' component={Combat} />
-                    <Route exact path='/configurebot/:socketId/guardjob' component={GuardJob} />
-                    <Route exact path='/configurebot/:socketId/minerjob' component={MinerJob} />
-                    <Redirect to='/dashboard' />
-                </Switch>
-            </ConfigureBotLayout>
+            <BrowserRouter basename="/configurebot">
+                <ConfigureBotLayout socketId={this.props.match.params.socketId}>
+                    <Switch>
+                        <Route exact path='/:socketId/generalconfig' component={GeneralConfig} />
+                        <Route exact path='/:socketId/itemstobeready' component={ItemsToBeReady} />
+                        <Route exact path='/:socketId/chests' component={Chests} />
+                        <Route exact path='/:socketId/combat' component={Combat} />
+                        <Route exact path='/:socketId/guardjob' component={GuardJob} />
+                        <Route exact path='/:socketId/minerjob' component={MinerJob} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </ConfigureBotLayout>
+            </BrowserRouter>
 
         );
     }
