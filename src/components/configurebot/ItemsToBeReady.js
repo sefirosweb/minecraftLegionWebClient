aa2a661f140e0699c44fd40789d8fb99e7739d9a
sefirosweb <sefirosweb@gmail.com>
2021-02-05 18:39:36 +0100

    Adding frontend itemToBeReady

--- src/components/configurebot/ItemsToBeReady.js
+++ src/components/configurebot/ItemsToBeReady.js
@@ -1,12 +1,93 @@
 import React from 'react'
-import '../../css/NotFound.css'
+import { Fragment } from 'react'
 
 class ItemsToBeReady extends React.Component {
-  render () {
+  render() {
     return (
-      <div className='not-found'>
-        <h1>ItemsToBeReady!</h1>
-      </div>
+      <Fragment>
+
+        <div className='card px-5 pt-4'>
+          <div className='row'>
+            <div className='col-12'>
+
+              <div className='row'>
+                <div className='col-12'>
+                  <label>
+                    This is a minimun requeried for start bot to work,<br />
+                  Example, Guard need a sword and shield, Miner need a 1 pickaxe
+                  </label>
+                </div>
+              </div>
+
+              <div className='row'>
+
+                <div className='col-6'>
+                  <div class="form-group">
+                    <label for="inputItem">Select Item</label>
+                    <select class="form-control" id="inputItem">
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div className='col-2'>
+                  <div class="form-group">
+                    <label for="inputQuantity">Quantity</label>
+                    <input type="text" class="form-control" id="inputQuantity" />
+                  </div>
+                </div>
+
+                <div className='col-2'>
+                  <div class="form-group">
+                    <label for="inputQuantity">.</label>
+                    <button className='form-control btn btn-primary'>Insert</button>
+                  </div>
+                </div>
+
+              </div>
+              <div className='row mt-4'>
+                <div className='col-12'>
+
+                  <table class="table">
+                    <thead class="thead-dark">
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Item</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col"></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>Delete</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>Delete</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>Delete</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Fragment >
     )
   }
 }
