5414fb8ce4085362500a6fa5b363ea1ce0b0df21
sefirosweb <sefirosweb@gmail.com>
2021-02-06 15:04:20 +0100

    Refactorized code

--- src/components/configurebot/GeneralConfig.js
+++ src/components/configurebot/GeneralConfig.js
@@ -1,51 +1,50 @@
-import React from 'react'
 import { Fragment } from 'react'
-class GeneralConfig extends React.Component {
-  render() {
-    return (
-      <Fragment>
-          <div className='row'>
-            <div className='col-12'>
-              <form>
 
-                <fieldset className="form-group row">
-                  <legend className="col-form-label col-sm-2 float-sm-left pt-0">Job</legend>
-                  <div className="col-sm-10">
-                    <div className="form-check">
-                      <input className="form-check-input" type="radio" name="gridJob" id="jobNone" value="none" />
-                      <label className="form-check-label" for="jobNone">None</label>
-                    </div>
-                    <div className="form-check">
-                      <input className="form-check-input" type="radio" name="gridJob" id="jobMiner" value="miner" />
-                      <label className="form-check-label" for="jobMiner">Miner</label>
-                    </div>
-                    <div className="form-check">
-                      <input className="form-check-input" type="radio" name="gridJob" id="jobGuard" value="guard" />
-                      <label className="form-check-label" for="jobGuard">Guard</label>
-                    </div>
+const GeneralConfig = (props) => {
+  return (
+    <Fragment>
+      <div className='row'>
+        <div className='col-12'>
+          <form>
 
-                  </div>
-                </fieldset>
+            <fieldset className="form-group row">
+              <legend className="col-form-label col-sm-2 float-sm-left pt-0">Job</legend>
+              <div className="col-sm-10">
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" name="gridJob" id="jobNone" value="none" />
+                  <label className="form-check-label" for="jobNone">None</label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" name="gridJob" id="jobMiner" value="miner" />
+                  <label className="form-check-label" for="jobMiner">Miner</label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" name="gridJob" id="jobGuard" value="guard" />
+                  <label className="form-check-label" for="jobGuard">Guard</label>
+                </div>
 
-                <fieldset className="form-group row">
-                  <legend className="col-form-label col-sm-2 float-sm-left pt-0">Pick up items?</legend>
-                  <div className="col-sm-10">
-                    <div className="form-check">
-                      <input className="form-check-input" type="radio" name="gridPickupItems" id="pickUpItemsTrue" value="yes" checked />
-                      <label className="form-check-label" for="pickUpItemsTrue">Yes</label>
-                    </div>
-                    <div className="form-check">
-                      <input className="form-check-input" type="radio" name="gridPickupItems" id="pickUpItemsFalse" value="no" />
-                      <label className="form-check-label" for="pickUpItemsFalse">no</label>
-                    </div>
-                  </div>
-                </fieldset>
-              </form>
-            </div>
-          </div>
-      </Fragment >
-    )
-  }
+              </div>
+            </fieldset>
+
+            <fieldset className="form-group row">
+              <legend className="col-form-label col-sm-2 float-sm-left pt-0">Pick up items?</legend>
+              <div className="col-sm-10">
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" name="gridPickupItems" id="pickUpItemsTrue" value="yes" checked />
+                  <label className="form-check-label" for="pickUpItemsTrue">Yes</label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" name="gridPickupItems" id="pickUpItemsFalse" value="no" />
+                  <label className="form-check-label" for="pickUpItemsFalse">no</label>
+                </div>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </Fragment >
+  )
 }
+
 
 export default GeneralConfig
--- src/components/configurebot/ItemsToBeReady.js
+++ src/components/configurebot/ItemsToBeReady.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
