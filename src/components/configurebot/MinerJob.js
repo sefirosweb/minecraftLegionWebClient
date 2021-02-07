3c0078a69771c5bbe983a72ea8b6e44d93dd485f
sefirosweb <sefirosweb@gmail.com>
2021-02-07 17:18:29 +0100

    Added miner dynamic

--- src/components/configurebot/MinerJob.js
+++ src/components/configurebot/MinerJob.js
@@ -1,7 +1,56 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
+import { connect } from 'react-redux'
+import { getBotBySocketId } from '../../actions/botsAction'
 import House_XYZ from '../../images/House_XYZ.png'
 
 const MinerJob = (props) => {
+
+  const [botConfig] = useState(props.getBotBySocketId(props.match.params.socketId))
+  if (botConfig === undefined) {
+    props.history.push('/dashboard')
+    return null
+  }
+
+  const handleChangeTunnel = (event) => {
+    props.socket.emit('sendAction', {
+      action: 'changeConfig',
+      socketId: botConfig.socketId,
+      value: {
+        configToChange: 'changeTunnel',
+        value: event.target.value
+      }
+    })
+  }
+
+
+  const handleChangeOrientation = (event) => {
+    props.socket.emit('sendAction', {
+      action: 'changeConfig',
+      socketId: botConfig.socketId,
+      value: {
+        configToChange: 'changeOrientation',
+        value: event.target.value
+      }
+    })
+  }
+
+
+  const handleChangePosMiner = (event) => {
+    const pos = event.target.value
+    const coord = event.target.id
+    props.socket.emit('sendAction', {
+      action: 'changeConfig',
+      socketId: botConfig.socketId,
+      value: {
+        configToChange: 'changePosMiner',
+        value: {
+          coord,
+          pos
+        }
+      }
+    })
+  }
+
   return (
     <Fragment>
 
@@ -20,11 +69,11 @@ const MinerJob = (props) => {
               <legend className="col-form-label col-sm-3 float-sm-left pt-0">Tunel type?</legend>
               <div className="col-sm-9">
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="tunnelType" value="yes" />
+                  <input className="form-check-input" type="radio" name="tunnelType" value="vertically" onChange={handleChangeTunnel} checked={botConfig.config.minerCords.tunel === 'vertically'} />
                   <label className="form-check-label">Make a Hole</label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="tunnelType" value="no" />
+                  <input className="form-check-input" type="radio" name="tunnelType" value="horizontally" onChange={handleChangeTunnel} checked={botConfig.config.minerCords.tunel === 'horizontally'} />
                   <label className="form-check-label">Make a Tunel</label>
                 </div>
               </div>
@@ -34,22 +83,22 @@ const MinerJob = (props) => {
         <div className='col-6'>
           <form>
             <fieldset className="form-group row">
-              <legend className="col-form-label col-sm-3 float-sm-left pt-0">Horientation?</legend>
+              <legend className="col-form-label col-sm-3 float-sm-left pt-0">Orientation?</legend>
               <div className="col-sm-9">
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="onrientationType" value="yes" />
+                  <input className="form-check-input" type="radio" name="onrientationType" value="x+" onChange={handleChangeOrientation} checked={botConfig.config.minerCords.orientation === 'x+'} />
                   <label className="form-check-label">X+</label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="onrientationType" value="no" />
+                  <input className="form-check-input" type="radio" name="onrientationType" value="x-" onChange={handleChangeOrientation} checked={botConfig.config.minerCords.orientation === 'x-'} />
                   <label className="form-check-label">X-</label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="onrientationType" value="no" />
+                  <input className="form-check-input" type="radio" name="onrientationType" value="z+" onChange={handleChangeOrientation} checked={botConfig.config.minerCords.orientation === 'z+'} />
                   <label className="form-check-label">Z+</label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="gridPionrientationTypeckupItems" value="no" />
+                  <input className="form-check-input" type="radio" name="onrientationType" value="z-" onChange={handleChangeOrientation} checked={botConfig.config.minerCords.orientation === 'z-'} />
                   <label className="form-check-label">Z-</label>
                 </div>
               </div>
@@ -67,21 +116,21 @@ const MinerJob = (props) => {
               <div className="form-group row">
                 <label className="col-sm-1 col-form-label">X</label>
                 <div className="col-sm-3">
-                  <input type="text" className="form-control" />
+                  <input type="text" className="form-control" id='xStart' onChange={handleChangePosMiner} value={botConfig.config.minerCords.xStart} />
                 </div>
               </div>
 
               <div className="form-group row">
                 <label className="col-sm-1 col-form-label">Y</label>
                 <div className="col-sm-3">
-                  <input type="text" className="form-control" />
+                  <input type="text" className="form-control" id='yStart' onChange={handleChangePosMiner} value={botConfig.config.minerCords.yStart} />
                 </div>
               </div>
 
               <div className="form-group row">
                 <label className="col-sm-1 col-form-label">Z</label>
                 <div className="col-sm-3">
-                  <input type="text" className="form-control" />
+                  <input type="text" className="form-control" id='zStart' onChange={handleChangePosMiner} value={botConfig.config.minerCords.zStart} />
                 </div>
               </div>
 
@@ -96,21 +145,21 @@ const MinerJob = (props) => {
               <div className="form-group row">
                 <label className="col-sm-1 col-form-label">X</label>
                 <div className="col-sm-3">
-                  <input type="text" className="form-control" />
+                  <input type="text" className="form-control" id='xEnd' onChange={handleChangePosMiner} value={botConfig.config.minerCords.xEnd} />
                 </div>
               </div>
 
               <div className="form-group row">
                 <label className="col-sm-1 col-form-label">Y</label>
                 <div className="col-sm-3">
-                  <input type="text" className="form-control" />
+                  <input type="text" className="form-control" id='yEnd' onChange={handleChangePosMiner} value={botConfig.config.minerCords.yEnd} />
                 </div>
               </div>
 
               <div className="form-group row">
                 <label className="col-sm-1 col-form-label">Z</label>
                 <div className="col-sm-3">
-                  <input type="text" className="form-control" />
+                  <input type="text" className="form-control" id='zEnd' onChange={handleChangePosMiner} value={botConfig.config.minerCords.zEnd} />
                 </div>
               </div>
 
@@ -130,4 +179,16 @@ const MinerJob = (props) => {
   )
 }
 
-export default MinerJob
+const mapStateToProps = (reducers) => {
+  const { botsReducer, configurationReducer } = reducers
+  const { botsOnline } = botsReducer
+  const { socket } = configurationReducer
+
+  return { socket, botsOnline }
+}
+
+const mapDispatchToProps = {
+  getBotBySocketId
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MinerJob);
