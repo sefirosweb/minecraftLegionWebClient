f22bd242b913a0e71a7d3368e8a5080c55ab0d34
sefirosweb <sefirosweb@gmail.com>
2021-02-07 12:43:57 +0100

    Added dynamic generalConfig

--- src/components/configurebot/GeneralConfig.js
+++ src/components/configurebot/GeneralConfig.js
@@ -1,7 +1,24 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
+import { connect } from 'react-redux'
+import { getBotBySocketId } from '../../actions/botsAction'
 
 const GeneralConfig = (props) => {
+  const [botConfig, setBotConfig] = useState(props.getBotBySocketId(props.match.params.socketId))
+
+  const handleChangeJob = (event) => {
+    props.socket.emit('sendAction', {
+      action: 'changeConfig',
+      socketId: botConfig.socketId,
+      value: {
+        configToChange: 'job',
+        value: event.target.value
+      }
+    })
+
+  }
+
   return (
+
     <Fragment>
       <div className='row'>
         <div className='col-12'>
@@ -11,15 +28,15 @@ const GeneralConfig = (props) => {
               <legend className="col-form-label col-sm-2 float-sm-left pt-0">Job</legend>
               <div className="col-sm-10">
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="gridJob" value="none" />
+                  <input className="form-check-input" type="radio" name="gridJob" value="none" onChange={handleChangeJob} checked={botConfig.config.job === "none"} />
                   <label className="form-check-label">None</label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="gridJob" value="miner" />
+                  <input className="form-check-input" type="radio" name="gridJob" value="miner" onChange={handleChangeJob} checked={botConfig.config.job === "miner"} />
                   <label className="form-check-label">Miner</label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="gridJob" value="guard" />
+                  <input className="form-check-input" type="radio" name="gridJob" value="guard" onChange={handleChangeJob} checked={botConfig.config.job === "guard"} />
                   <label className="form-check-label">Guard</label>
                 </div>
 
@@ -46,5 +63,16 @@ const GeneralConfig = (props) => {
   )
 }
 
+const mapStateToProps = (reducers) => {
+  const { botsReducer, configurationReducer } = reducers
+  const { botsOnline } = botsReducer
+  const { socket } = configurationReducer
 
-export default GeneralConfig
+  return { socket, botsOnline }
+}
+
+const mapDispatchToProps = {
+  getBotBySocketId
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GeneralConfig);
