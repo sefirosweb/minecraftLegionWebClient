22f699472a68658c1193cc86ccb62b0fa3bd6fec
sefirosweb <sefirosweb@gmail.com>
2021-02-07 01:51:08 +0100

    Added combat front end

--- src/components/configurebot/Combat.js
+++ src/components/configurebot/Combat.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import '../../css/NotFound.css'
+import { Fragment } from "react"
 
-class Combat extends React.Component {
-  render () {
-    return (
-      <div className='not-found'>
-        <h1>Combat!</h1>
+const Combat = (props) => {
+  return (
+    <Fragment>
+      <div className='row'>
+        <div className='col-6'>
+          <form>
+
+            <fieldset className="form-group row">
+              <legend className="col-form-label col-sm-4 float-sm-left pt-0">Combat Mode?</legend>
+              <div className="col-sm-8">
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" name="combatMode" value="none" />
+                  <label className="form-check-label">None</label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" name="combatMode" value="miner" />
+                  <label className="form-check-label">PVE</label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" name="combatMode" value="guard" />
+                  <label className="form-check-label">PVP</label>
+                </div>
+
+              </div>
+            </fieldset>
+
+          </form>
+        </div>
+        <div className='col-3'>
+          <div class="form-group">
+            <label for="inputItem">Distance for start combat?</label>
+            <input className='form-control' type="text" />
+          </div>
+        </div>
       </div>
-    )
-  }
+    </Fragment>
+  )
 }
-
 export default Combat
