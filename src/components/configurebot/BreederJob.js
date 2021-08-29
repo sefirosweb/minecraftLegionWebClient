5ff880f5f6d7f6e57e600b3867d972fa86ed7a69
Sefirosweb <sefirosweb@gmail.com>
2021-08-29 15:15:09 +0200

    Added all animals

--- src/components/configurebot/BreederJob.js
+++ src/components/configurebot/BreederJob.js
@@ -57,18 +57,78 @@ const BreederJob = (props) => {
                 <input type="text" className="form-control" value={botConfig.config.farmAnimal.seconds} onChange={handleUpdateAnimal.bind(props, 'seconds')} />
               </div>
             </div>
+
             <div className="form-group row">
               <label className="col-sm-2 col-form-label">Cow</label>
               <div className="col-sm-2">
                 <input type="text" className="form-control" value={botConfig.config.farmAnimal.cow} onChange={handleUpdateAnimal.bind(props, 'cow')} />
               </div>
-            </div>
-            <div className="form-group row">
               <label className="col-sm-2 col-form-label">Sheep</label>
               <div className="col-sm-2">
                 <input type="text" className="form-control" value={botConfig.config.farmAnimal.sheep} onChange={handleUpdateAnimal.bind(props, 'sheep')} />
               </div>
+              <label className="col-sm-2 col-form-label">Chicken</label>
+              <div className="col-sm-2">
+                <input type="text" className="form-control" value={botConfig.config.farmAnimal.chicken} onChange={handleUpdateAnimal.bind(props, 'chicken')} />
+              </div>
             </div>
+
+            <div className="form-group row">
+              <label className="col-sm-2 col-form-label">Horse</label>
+              <div className="col-sm-2">
+                <input type="text" className="form-control" value={botConfig.config.farmAnimal.horse} onChange={handleUpdateAnimal.bind(props, 'horse')} />
+              </div>
+              <label className="col-sm-2 col-form-label">Donkey</label>
+              <div className="col-sm-2">
+                <input type="text" className="form-control" value={botConfig.config.farmAnimal.donkey} onChange={handleUpdateAnimal.bind(props, 'donkey')} />
+              </div>
+              <label className="col-sm-2 col-form-label">Llama</label>
+              <div className="col-sm-2">
+                <input type="text" className="form-control" value={botConfig.config.farmAnimal.llama} onChange={handleUpdateAnimal.bind(props, 'llama')} />
+              </div>
+            </div>
+
+            <div className="form-group row">
+              <label className="col-sm-2 col-form-label">Fox</label>
+              <div className="col-sm-2">
+                <input type="text" className="form-control" value={botConfig.config.farmAnimal.fox} onChange={handleUpdateAnimal.bind(props, 'fox')} />
+              </div>
+              <label className="col-sm-2 col-form-label">Bee</label>
+              <div className="col-sm-2">
+                <input type="text" className="form-control" value={botConfig.config.farmAnimal.bee} onChange={handleUpdateAnimal.bind(props, 'bee')} />
+              </div>
+              <label className="col-sm-2 col-form-label">Panda</label>
+              <div className="col-sm-2">
+                <input type="text" className="form-control" value={botConfig.config.farmAnimal.panda} onChange={handleUpdateAnimal.bind(props, 'panda')} />
+              </div>
+            </div>
+            
+            <div className="form-group row">
+              <label className="col-sm-2 col-form-label">Wolf</label>
+              <div className="col-sm-2">
+                <input type="text" className="form-control" value={botConfig.config.farmAnimal.wolf} onChange={handleUpdateAnimal.bind(props, 'wolf')} />
+              </div>
+              <label className="col-sm-2 col-form-label">Cat</label>
+              <div className="col-sm-2">
+                <input type="text" className="form-control" value={botConfig.config.farmAnimal.cat} onChange={handleUpdateAnimal.bind(props, 'cat')} />
+              </div>
+            </div>
+
+            <div className="form-group row">
+              <label className="col-sm-2 col-form-label">Pig</label>
+              <div className="col-sm-2">
+                <input type="text" className="form-control" value={botConfig.config.farmAnimal.pig} onChange={handleUpdateAnimal.bind(props, 'pig')} />
+              </div>
+              <label className="col-sm-2 col-form-label">Turtles</label>
+              <div className="col-sm-2">
+                <input type="text" className="form-control" value={botConfig.config.farmAnimal.turtles} onChange={handleUpdateAnimal.bind(props, 'turtles')} />
+              </div>
+
+            </div>
+
+
+   
+
           </form>
         </div>
       </div>
