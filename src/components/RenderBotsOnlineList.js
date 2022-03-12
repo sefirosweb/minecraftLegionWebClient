6e5110d8e39633fdcdc94525f35af65bd64439cb
Sefirosweb <sefirosweb@gmail.com>
2022-03-12 23:37:47 +0100

    Refactorized

--- src/components/RenderBotsOnlineList.js
+++ src/components/RenderBotsOnlineList.js
@@ -4,11 +4,11 @@ import '../css/botlist.css'
 import ProgressBar from 'react-bootstrap/ProgressBar'
 import { setSelectedSocketId } from '../actions/configurationAction'
 
-const RenderBotsOnlineList = (props) => {
+const RenderBotsOnlineList = ({ selectedSocketId, setSelectedSocketId, botsOnline }) => {
   useEffect(() => {
     const handleEsc = (event) => {
       if (event.keyCode === 27) {
-        props.setSelectedSocketId(undefined)
+        setSelectedSocketId(undefined)
       }
     }
     window.addEventListener('keydown', handleEsc)
@@ -16,15 +16,15 @@ const RenderBotsOnlineList = (props) => {
     return () => {
       window.removeEventListener('keydown', handleEsc)
     }
-  }, [props])
+  }, [setSelectedSocketId])
 
   const renderBotList = () => {
     return (
-      props.botsOnline.map((bot) => {
+      botsOnline.map((bot) => {
         return (
           <li key={bot.socketId} className={`list-group-item ${(bot.combat) ? 'botlistCombat' : 'botlist'}`}>
             <div className={` ${(bot.combat) ? 'botCombat' : ''}`}>
-              <span className={`pointer ${props.selectedSocketId === bot.socketId ? 'is-selected' : ''}`} onClick={() => { props.setSelectedSocketId(bot.socketId) }}>{bot.name}</span>
+              <span className={`pointer ${selectedSocketId === bot.socketId ? 'is-selected' : ''}`} onClick={() => { setSelectedSocketId(bot.socketId) }}>{bot.name}</span>
               <div>
                 <ProgressBar className='mt-1' variant='danger' now={bot.health / 20 * 100} />
                 <ProgressBar className='mt-1' variant='warning' now={bot.food / 20 * 100} />
@@ -39,7 +39,7 @@ const RenderBotsOnlineList = (props) => {
 
   return (
     <ul className='list-group'>
-      <li className='list-group-item active'>Bots Online ({props.botsOnline.length})</li>
+      <li className='list-group-item active'>Bots Online ({botsOnline.length})</li>
       {renderBotList()}
     </ul>
   )
