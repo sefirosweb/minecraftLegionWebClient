dbeaa285759c33d3e5280d9061d7e31cd6cea2d4
Sefirosweb <sefirosweb@gmail.com>
2023-02-01 12:02:16 +0100

    Installed typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './routes/App'
-import reportWebVitals from './reportWebVitals'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './routes/App';
+import reportWebVitals from './reportWebVitals';
 
-// Para conectar todos los reducers
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reduxThunk from 'redux-thunk'
-
 import reducers from './reducers/reducers_index'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+
+
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = createRoot(rootElement);
+
 
 const store = createStore(
   reducers, // Todos los reducers
   {}, // Estado inicial
   applyMiddleware(reduxThunk)
 )
-
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
